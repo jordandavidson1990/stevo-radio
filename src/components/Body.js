5b19d5402368f64d1818399592375387deace87e
Jordan Davidson <jordandavidson1990@hotmail.com>
2020-03-31 21:27:05 +0100

    updated package.json to include engines

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ import "./Body.css";
 export default function Body() {
   return (
     <div className="audio-player-container">
-      <audio controls className="audio-player">
+      <audio controls className="audio-player" autoPlay="false">
         <source src="brothers.mp3" type="audio/mpeg" />
       </audio>
     </div>
